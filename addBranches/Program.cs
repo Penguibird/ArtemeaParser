9e627488d2042edcf8b1f731bbaa5132b5370219
Penguibird <loskotaklp@gmail.com>
2021-05-13 10:48:51 +0200

    initial

--- addBranches/Program.cs
+++ addBranches/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,49 +24,133 @@ namespace addBranches
             string dbName = "artemea_prod";
             ArtemeaDb _dbContext = new ArtemeaDb(dbString, dbName);
             Program p = new Program();
+            var positions = new List<Position>();
+            var branches = new List<Branch>();
+            var skills = new List<Skill>();
+            var trainings = new List<Training>();
 
-            String[] names = { "Integration Technical Specialist",
-             "Java Midleware technical specialist",
-              "Unix Technical Specialist",
-               "ERP Specialist",
-                "Server Implementation Specialist",
-                 "Technical specialist - Public Cloud",
-                  "Backup Technical Specialist",
-                  "Storage Technical Specialist",
-                  "EDM Technical Specialist",
-                   };
-            Guid parentID = new Guid("c03c43ae-e3e0-48e6-a488-fdf2985d5a99");
-            Branch parent = new Branch()
+            using (var reader = new StreamReader(@"D:\scripts\ArtemeaParser\addBranches\map.csv"))
             {
-                ID = parentID,
-                ChildrenIDs = new List<Guid>(),
-                name = "Linux",
-                positionIDs = new List<Guid>(),
-            };
-            foreach (String name in names)
-            {
-                Branch branch = (await _dbContext.FindByAttribute<Branch, String>("branches", "name", name))[0];
-                branch.parentID = parentID;
-                parent.ChildrenIDs.Add(branch.ID);
-                await _dbContext.Replace<Branch>("branches", branch.ID, branch);
-                Console.WriteLine(parentID);
+                while (!reader.EndOfStream)
+                {
+                    String line = reader.ReadLine();
+                    String[] values = line.Split(',');
+                    String unit = values[1];
+                    String parentBranchName = unit;
+                    String team = values[2];
+                    String branchName = values[3]; // job role in the xls
+                    String product = values[4];
+                    String skillName = values[5]; //
+                    if (new Regex("(Skills)|(Trainings)", RegexOptions.IgnoreCase).IsMatch(skillName))
+                    {
+                        skillName = values[4];
+                    }
+                    List<String> skillLevels = values.ToList().GetRange(6, values.Length - 6);
+                    skillLevels = skillLevels.ToList().Where(_ => p.isValidField(_)).ToList();
+                    Skill skill = null;
+
+
+                    skill = skills.Find(_ => _.Name == skillName);
+
+                    if (skill == null)
+                    {
+                        skill = new Skill
+                        {
+                            ID = Guid.NewGuid(),
+                            Name = skillName,
+                            Description = "",
+                            Levels = skillLevels,
+                            Category = SkillCategory.Hard,
+                            Deleted = false,
+                        };
+                        skills.Add(skill);
+                    }
+                    Branch parentBranch = null;
+
+
+                    parentBranch = branches.Find(_ => _.name == parentBranchName);
+
+                    if (parentBranch == null)
+                    {
+                        parentBranch = new Branch
+                        {
+                            ID = Guid.NewGuid(),
+                            name = parentBranchName,
+                            parentID = Guid.Empty, //add superparent
+                            positionIDs = new List<Guid>(),
+                            ChildrenIDs = new List<Guid>(),
+                        };
+                        branches.Add(parentBranch);
+                    }
+                    Branch branch = null;
+
+
+                    branch = branches.Find(_ => _.name == branchName);
+
+                    if (branch == null)
+                    {
+                        branch = new Branch
+                        {
+                            ID = Guid.NewGuid(),
+                            name = branchName,
+                            parentID = parentBranch.ID,
+                            positionIDs = new List<Guid>(),
+                            ChildrenIDs = new List<Guid>(),
+                        };
+                        parentBranch.ChildrenIDs.Add(branch.ID);
+                        branches.Add(branch);
+                    }
+                    for (int i = 1; i < values.Length - 5; i++)
+                    {
+                        TierNameMapping t = new TierNameMapping(i);
+                        if (!p.isValidField(values[t.csvIndex])) continue;
+                        String positionName = branchName + "" + t.nameSuffix;
+                        Position position = positions.Find(_ => _.Name == positionName);
+
+                        if (position == null)
+                        {
+                            position = new Position
+                            {
+                                ID = Guid.NewGuid(),
+                                Name = positionName,
+                                Tier = t.Tier,
+                                Description = "",
+                                RequiredSkills = new List<PositionSkill>(),
+                                branchID = branch.ID,
+                            };
+                            branch.positionIDs.Add(position.ID);
+                            positions.Add(position);
+                        }
+                        int skillLevel = skill.Levels.FindIndex(_ => _ == values[t.csvIndex]);
+                        position.RequiredSkills.Add(new PositionSkill
+                        {
+                            SkillId = skill.ID,
+                            Level = skillLevel,
+                        });
+                    }
+                    Console.WriteLine("Parsed a new line, Skill:", skillName);
+                }
             }
-            _dbContext.Add("branches", parent);
+            // var dbSnapshotPositions = await _dbContext.GetList<Position>("positions");
+            // var dbSnapshotSkills = 
+            foreach (Position position in positions)
+            {
+                await _dbContext.Add<Position>("positions", position);
+            }
+            foreach (Branch branch in branches)
+            {
+                await _dbContext.Add<Branch>("branches", branch);
+            }
+            foreach (Skill skill in skills)
+            {
+                await _dbContext.Add<Skill>("skills", skill);
+            }
+            Console.ReadLine();
+        }
 
-
-
-            // foreach (Branch branch in branches)
-            // {
-            //     branch.positions = new List<Position>();
-            //     foreach (Guid id in branch.positionIDs)
-            //     {
-            //         branch.positions.Add(await _dbContext.FindById<Position>("positions", id));
-            //     }
-            //     List<Guid> posIds = branch.positionIDs.ToList<Guid>();
-            //     posIds.Sort(delegate (Guid id1, Guid id2) { return p.sortingHelper(id1, id2, branch.positions.ToList<Position>()); });
-            //     branch.positionIDs = posIds;
-            //     await _dbContext.Replace<Branch>("branches", branch.ID, branch);
-            // }
+        public bool isValidField(String _)
+        {
+            return (_ != "" && _ != null && !(new Regex("(not applicable)|(N.?A)", RegexOptions.IgnoreCase).IsMatch(_)));
         }
 
         public int sortingHelper(Guid id1, Guid id2, List<Position> positions)
---
+++ addBranches/TierNameMapping.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace Data.Entities
+{
+    public class TierNameMapping
+    {
+        public TierNameMapping(int index)
+        {
+            csvIndex = index + 5;
+            if (index <= 5)
+            {
+                nameSuffix = "TS " + index;
+            }
+            else
+            {
+                nameSuffix = "IA " + (index - 5);
+            }
+            if (index <= 3)
+            {
+                Tier = 2;
+            }
+            else if (index > 7)
+            {
+                Tier = 4;
+            }
+            else
+            {
+                Tier = 3;
+            }
+        }
+        public int csvIndex { get; set; }
+        public int Tier { get; set; }
+        public String nameSuffix { get; set; }
+    }
+}
